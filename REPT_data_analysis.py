8aae0f4fd6c63f6aeef50b6ea63facac7feab181
Will Teague <wzt0020@auburn.edu>
2025-01-24 15:57:05 -0600

    all energy channels on the same plot with same labels

--- REPT_data_analysis.py
+++ REPT_data_analysis.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
+textsize = 16
+
 #file_path = "C:/Users/Will/Box/HERT_Box/REPT Data/April 2017 Storm/rbspa_rel03_ect-rept-sci-l2_20170421_v5.4.0.cdf"
 file_path = "C:/Users/wzt0020/Box/HERT_Box/REPT Data/April 2017 Storm/rbspa_rel03_ect-rept-sci-l2_20170421_v5.4.0.cdf"
 cdf_data = pycdf.CDF(file_path)
@@ -18,30 +20,39 @@ if fesa_min<1:
 norm = colors.LogNorm(vmin=fesa_min, vmax= np.max(FESA))
 energy_channels = cdf_data["FESA_Energy"][:]
 
+# Create the figure with subplots
+fig, axes = plt.subplots(len(energy_channels), 1, figsize=(16, 40), sharex=True)
 
-for i in range(energy_channels.shape[0]):
-    # Set the figure size (width, height) in inches
-    plt.figure(figsize=(16, 4)) 
+# Loop through each energy channel
+for i, ax in enumerate(axes.flat):
+  # Create the scatter plot on the current subplot
+  subplot = ax.scatter(Epoch, L, c=FESA[:, i], cmap='viridis', norm=norm)
+
+  # Add labels and title
+  ax.set_ylabel('L', fontsize=textsize)
+  ax.set_title(f'RBSP-A REPT {energy_channels[i]:.2f} MeV Electron Spin-Averaged Flux', fontsize=textsize)
+
+  ax.tick_params(axis='both', which='major', labelsize=textsize)
+  ax.set_ylim(0, 7)
+  ax.grid(True)
+
+# Add x-axis label for last plot
+ax.set_xlabel('UTC', fontsize=textsize)
+
+# Remove extra subplots if there aren't enough energy channels
+if len(energy_channels) < len(axes.flat):
+  for ax in axes.flat[len(energy_channels):]:
+    fig.delaxes(ax)
+
+# Create a single colorbar outside the loop for efficiency
+fig.subplots_adjust(right=0.9)
+cbar_ax = fig.add_axes([0.95, 0.15, 0.02, 0.7])
+cbar = fig.colorbar(subplot, cax=cbar_ax, label='Flux')
+cbar.set_label(label = 'Flux', fontsize=textsize)
+cbar.ax.tick_params(labelsize=textsize) 
+
+# Show the plot
+plt.show()
     
-    # Create the scatter plot
-    plt.scatter(Epoch, L, c=FESA[:,i], cmap='viridis', norm=norm) 
-    
-    # Add labels and title
-    plt.xlabel('UTC', fontsize=12)
-    plt.ylabel('L', fontsize=12)
-    plt.title(f'RBSP-A REPT {energy_channels[i]:.2f} MeV Electron Spin-Averaged Flux', fontsize=16)
-    
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-    plt.ylim(0, 7) 
-    
-    # Add grid lines
-    plt.grid(True) 
-    
-    # Add colorbar
-    cbar = plt.colorbar()
-    cbar.set_label(label = 'Flux', fontsize=12)
-    cbar.ax.tick_params(labelsize=12) 
-    
-    # Show the plot
-    plt.show()
+# Close the CDF file
+cdf_data.close()
