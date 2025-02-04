af808797ce31bfedfbbc1e7271b1335df283db4a
Will Teague <wzt0020@auburn.edu>
2025-02-04 11:17:23 -0600

    attempted interpolation methods

--- REPT_data_analysis.py
+++ REPT_data_analysis.py
@@ -8,50 +8,43 @@ import glob
 textsize = 16
 
 # Folder containing CDF files
-folder_path = "C:/Users/Will/Box/Multipoint_Box/REPT Data/April 2017 Storm/"
-ephemeris_path = "C:/Users/Will/Box/Multipoint_Box/REPT Data/April 2017 Storm/ephemeris/"
+folder_path = "C:/Users/wzt0020/Box/Multipoint_Box/REPT Data/April 2017 Storm/"
+ephemeris_path = "C:/Users/wzt0020/Box/Multipoint_Box/REPT Data/April 2017 Storm/ephemeris/"
 
 # Get all CDF file paths in the folder
-#cdf_file_paths = glob.glob(folder_path + "rbspa*[!r]*.cdf") 
-cdf_file_paths = glob.glob(folder_path + "*.cdf") 
-ephem_file_paths = glob.glob(ephemeris_path + "*.cdf")
+#cdf_file_paths = glob.glob(folder_path + "*.cdf") 
+cdf_file_paths_A = glob.glob(folder_path + "rbspa*[!r]*.cdf") 
+cdf_file_paths_B = glob.glob(folder_path + "rbspb*[!r]*.cdf") 
+#ephem_file_paths = glob.glob(ephemeris_path + "*.cdf")
+ephem_file_paths_A = glob.glob(ephemeris_path + "rbsp-a*[!r]*.cdf")
+ephem_file_paths_B = glob.glob(ephemeris_path + "rbsp-b*[!r]*.cdf")
 
-# Initialize empty dictionaries to store data from all files (separate for A and B)
-Epoch_A = []
-L_A = []
-FESA_A = None
 
-Epoch_B = []
-L_B = []
-FESA_B = None
+# Function for reading in RBSP flux data
+def process_flux_data(file_paths):
+    Epoch = []
+    L = []
+    FESA = None
+    for file_path in file_paths:
+        # Extract filename without path
+        file_name = os.path.basename(file_path)
+        print(f"Processing file: {file_name}")
+        # Load the CDF data
+        cdf_data = pycdf.CDF(file_path)
 
-for file_path in cdf_file_paths:
-    # Extract filename without path
-    file_name = os.path.basename(file_path)
-    print(f"Processing file: {file_name}")
-    # Load the CDF data
-    cdf_data = pycdf.CDF(file_path)
-
-    # Separate data based on filename prefix
-    if file_name.startswith("rbspa"):
-        # Store data in A variables
-        Epoch_A.extend(cdf_data["Epoch"][:])
-        L_A.extend(cdf_data["L"][:])
-        if FESA_A is None:
-            FESA_A = cdf_data["FESA"][:]
+        Epoch.extend(cdf_data["Epoch"][:])
+        L.extend(cdf_data["L"][:])
+        if FESA is None:
+            FESA = cdf_data["FESA"][:]
             energy_channels = cdf_data["FESA_Energy"][:]
         else:
-            FESA_A = np.vstack((FESA_A, cdf_data["FESA"][:]))
-    else:
-        # Store data in B variables
-        Epoch_B.extend(cdf_data["Epoch"][:])
-        L_B.extend(cdf_data["L"][:])
-        if FESA_B is None:
-            FESA_B = cdf_data["FESA"][:]
-            energy_channels = cdf_data["FESA_Energy"][:]
-        else:
-            FESA_B = np.vstack((FESA_B, cdf_data["FESA"][:]))
-    cdf_data.close()
+                FESA = np.vstack((FESA, cdf_data["FESA"][:]))
+        cdf_data.close()
+    return Epoch, L, FESA, energy_channels
+
+print("Processing Flux Data:")
+Epoch_A, L_A, FESA_A, energy_channels_A = process_flux_data(cdf_file_paths_A)
+Epoch_B, L_B, FESA_B, energy_channels_B = process_flux_data(cdf_file_paths_B)
 
 # Handle cases where only A or B data is present (check which lists are not empty)
 if not Epoch_A and not L_A and not FESA_A:
@@ -77,72 +70,58 @@ norm = colors.LogNorm(vmin=fesa_min, vmax= fesa_max)
 '''
 Interpolate Lm_eq from ephemeris data
 '''
-
-Epoch_ephem_A = []
-Lm_eq_A = []
-Epoch_ephem_B = []
-Lm_eq_B = []
-
-# Read in RBPS data
-for file_path in enumerate(ephem_file_paths):
-    # Extract filename without path
-    file_name = os.path.basename(file_path)
-    print(f"Processing file: {file_name}")
-    # Load the CDF data
-    ephem_data = pycdf.CDF(file_path)
-    
-    Epoch_ephem = ephem_data['Epoch'][:]
-    Lm_eq = ephem_data['Lm_eq'][:]
-    
-    # Separate data based on filename prefix
-    if file_name.startswith("rbsp-a"):
-        # Store data in A variables
+# Function for reading in RBSP ephemeris data
+def process_ephem_data(ephem_file_paths):
+    Epoch_ephem = []
+    Lm_eq = []
+    for f, file_path in enumerate(ephem_file_paths):
+        # Extract filename without path
+        file_name = os.path.basename(file_path)
+        print(f"Processing file: {file_name}")
+        # Load the CDF data
+        ephem_data = pycdf.CDF(file_path)
+        
         if f < len(ephem_file_paths) - 1:  # Exclude the last file for Epoch_ephem_A and Lm_eq_A
-            Epoch_ephem_A.extend(ephem_data['Epoch'][:-1]) 
-            Lm_eq_A.extend(ephem_data['Lm_eq'][:-1])
+            Epoch_ephem.extend(ephem_data['Epoch'][:-1]) 
+            Lm_eq.extend(ephem_data['Lm_eq'][:-1])
         else:
-            Epoch_ephem_A.extend(ephem_data['Epoch'][:])
-            Lm_eq_A.extend(ephem_data['Lm_eq'][:])
-    elif file_name.startswith("rbsp-b"):
-        # Store data in A variables
-        if f < len(ephem_file_paths) - 1:  # Exclude the last file for Epoch_ephem_A and Lm_eq_A
-            Epoch_ephem_B.extend(ephem_data['Epoch'][:-1]) 
-            Lm_eq_B.extend(ephem_data['Lm_eq'][:-1])
-        else:
-            Epoch_ephem_B.extend(ephem_data['Epoch'][:])
-            Lm_eq_B.extend(ephem_data['Lm_eq'][:])
-    ephem_data.close()
+            Epoch_ephem.extend(ephem_data['Epoch'][:])
+            Lm_eq.extend(ephem_data['Lm_eq'][:])
+        ephem_data.close()
+    return Epoch_ephem, Lm_eq
 
-# Interpolate for RBSP-A data
-if Epoch_ephem_A != []:
-    for t in range(len(Epoch_ephem_A)-1):
+print("Processing Ephemeris Data:")
+Epoch_ephem_A, Lm_eq_A = process_ephem_data(ephem_file_paths_A)
+Epoch_ephem_B, Lm_eq_B = process_ephem_data(ephem_file_paths_B)
+
+# Interpolate for RBSP data
+Epoch_A_float = [epoch_a.timestamp() for epoch_a in Epoch_A]
+Epoch_ephem_A_float = [epoch_ephem_a.timestamp() for epoch_ephem_a in Epoch_ephem_A]
+Lm_eq_A_interp = np.interp(Epoch_A_float, Epoch_ephem_A_float, Lm_eq_A)
+
+def interp_ephem_data(Epoch, Epoch_ephem, Lm_eq):
+    for t in range(len(Epoch_ephem)-1):
         #t1 = Epoch_ephem[t]                 #t2 = Epoch_ephem[t+1]
         #Lm_p1 = Lm_eq[t]                    #Lm_p2 = Lm_eq[t+1]
         # Calculate change in time between two points
-        delta_t = Epoch_ephem_A[t+1]-Epoch_ephem_A[t]
+        delta_t = Epoch_ephem[t+1]-Epoch_ephem[t]
         # Calculate slope of linear equations (convert time delta to float)
-        m = (Lm_eq_A[t+1]-Lm_eq_A[t])/delta_t.total_seconds()
+        m = (Lm_eq[t+1]-Lm_eq[t])/delta_t.total_seconds()
         
         # Caulcuate time since first point (set first point to t=0)
-        time_since_start = Epoch_ephem_A[t]-Epoch_ephem_A[0]
+        time_since_start = Epoch_ephem[t]-Epoch_ephem[0]
         # Calculate y-int of linear equations (convert time delta to float)
-        b = Lm_eq_A[t] - m * time_since_start.total_seconds()
-'''
-# Interpolate for RBSP-B data
-if Epoch_ephem_B != []:
-    for t in range(len(Epoch_ephem_B)-1):
-        #t1 = Epoch_ephem[t]                 #t2 = Epoch_ephem[t+1]
-        #Lm_p1 = Lm_eq[t]                    #Lm_p2 = Lm_eq[t+1]
-        # Calculate change in time between two points
-        delta_t = Epoch_ephem_B[t+1]-Epoch_ephem_B[t]
-        # Calculate slope of linear equations (convert time delta to float)
-        m = (Lm_eq_B[t+1]-Lm_eq_B[t])/delta_t.total_seconds()
-        
-        # Caulcuate time since first point (set first point to t=0)
-        time_since_start = Epoch_ephem_B[t]-Epoch_ephem_B[0]
-        # Calculate y-int of linear equations (convert time delta to float)
-        b = Lm_eq_B[t] - m * time_since_start.total_seconds()
-'''
+        b = Lm_eq[t] - m * time_since_start.total_seconds()
+        #Epoch_strip = [epoch.timestamp() for epoch in Epoch]
+        #Epoch_ephem_strip = [epoch_ephem.timestamp() for epoch_ephem in Epoch_ephem]
+        #Epoch_subset = Epoch_strip[(Epoch_strip >= Epoch_ephem_strip[t]) & (Epoch_strip <= Epoch_ephem_strip[t+1])] 
+        #Epoch_subset = Epoch[(Epoch >= Epoch_ephem[t]) & (Epoch_strip <= Epoch_ephem[t+1])]
+        indices = [i for i, epoch in enumerate(Epoch) if Epoch_ephem[t] <= epoch <= Epoch_ephem[t+1]]
+
+print("Interpolating McIlwain L:")
+interp_ephem_data(Epoch_A, Epoch_ephem_A, Lm_eq_A)
+interp_ephem_data(Epoch_B, Epoch_ephem_B, Lm_eq_B)
+
 '''
 Plot RBSP Flux Data with ephemeris Lm_eq
 '''
