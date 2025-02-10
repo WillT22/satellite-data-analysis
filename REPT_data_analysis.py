09fa4e259ca118482d2fe9d33896ef19f2396319
Will Teague <wzt0020@auburn.edu>
2025-02-10 10:19:14 -0600

    Completed interpolation of Lm_eq

--- REPT_data_analysis.py
+++ REPT_data_analysis.py
@@ -52,6 +52,22 @@ if not Epoch_A and not L_A and not FESA_A:
 if not Epoch_B and not L_B and not FESA_B:
     print("No RBSPB data found in the folder.")
     
+# Find the earliest and latest Epoch values
+if Epoch_A and Epoch_B: 
+    min_epoch = min(min(Epoch_A), min(Epoch_B))
+    max_epoch = max(max(Epoch_A), max(Epoch_B))
+else:
+    # Handle cases where either Epoch_A or Epoch_B is empty
+    if Epoch_A:
+        min_epoch = min(Epoch_A)
+        max_epoch = max(Epoch_A)
+    elif Epoch_B:
+        min_epoch = min(Epoch_B)
+        max_epoch = max(Epoch_B)
+    else:
+        min_epoch = None
+        max_epoch = None
+    
 # Calculate minimum and maximum FESA values across all channels
 # divide by 1000 for keV to compare to Zhao 2018
 if FESA_A is not None and FESA_B is not None:
@@ -95,60 +111,47 @@ Epoch_ephem_A, Lm_eq_A = process_ephem_data(ephem_file_paths_A)
 Epoch_ephem_B, Lm_eq_B = process_ephem_data(ephem_file_paths_B)
 
 # Interpolate for RBSP data
-Epoch_A_float = [epoch_a.timestamp() for epoch_a in Epoch_A]
-Epoch_ephem_A_float = [epoch_ephem_a.timestamp() for epoch_ephem_a in Epoch_ephem_A]
-Lm_eq_A_interp = np.interp(Epoch_A_float, Epoch_ephem_A_float, Lm_eq_A)
-
-def interp_ephem_data(Epoch, Epoch_ephem, Lm_eq):
-    for t in range(len(Epoch_ephem)-1):
-        #t1 = Epoch_ephem[t]                 #t2 = Epoch_ephem[t+1]
-        #Lm_p1 = Lm_eq[t]                    #Lm_p2 = Lm_eq[t+1]
-        # Calculate change in time between two points
-        delta_t = Epoch_ephem[t+1]-Epoch_ephem[t]
-        # Calculate slope of linear equations (convert time delta to float)
-        m = (Lm_eq[t+1]-Lm_eq[t])/delta_t.total_seconds()
-        
-        # Caulcuate time since first point (set first point to t=0)
-        time_since_start = Epoch_ephem[t]-Epoch_ephem[0]
-        # Calculate y-int of linear equations (convert time delta to float)
-        b = Lm_eq[t] - m * time_since_start.total_seconds()
-        #Epoch_strip = [epoch.timestamp() for epoch in Epoch]
-        #Epoch_ephem_strip = [epoch_ephem.timestamp() for epoch_ephem in Epoch_ephem]
-        #Epoch_subset = Epoch_strip[(Epoch_strip >= Epoch_ephem_strip[t]) & (Epoch_strip <= Epoch_ephem_strip[t+1])] 
-        #Epoch_subset = Epoch[(Epoch >= Epoch_ephem[t]) & (Epoch_strip <= Epoch_ephem[t+1])]
-        indices = [i for i, epoch in enumerate(Epoch) if Epoch_ephem[t] <= epoch <= Epoch_ephem[t+1]]
+def interpolate_Lm_eq(Epoch, Epoch_ephem, Lm_eq):
+    Epoch_float = [epoch.timestamp() for epoch in Epoch]
+    Epoch_ephem_float = [epoch_ephem.timestamp() for epoch_ephem in Epoch_ephem]
+    Lm_eq_interp = np.interp(Epoch_float, Epoch_ephem_float, Lm_eq)
+    return Lm_eq_interp
 
 print("Interpolating McIlwain L:")
-interp_ephem_data(Epoch_A, Epoch_ephem_A, Lm_eq_A)
-interp_ephem_data(Epoch_B, Epoch_ephem_B, Lm_eq_B)
+Lm_eq_A_interp = interpolate_Lm_eq(Epoch_A, Epoch_ephem_A, Lm_eq_A)
+Lm_eq_B_interp = interpolate_Lm_eq(Epoch_B, Epoch_ephem_B, Lm_eq_B)
+
 
 '''
 Plot RBSP Flux Data with ephemeris Lm_eq
 '''
+print("Plotting Data:")
 # Create a custom colormap based on 'nipy_spectral' to match with IDL rainbow
 cmap = plt.get_cmap('nipy_spectral') 
 new_cmap = cmap(np.linspace(0, 0.875, 256))  # Use only the first 87.5% of the colormap
 
 # Create a new colormap object
 custom_cmap = colors.ListedColormap(new_cmap)
-'''
+
 # Create the figure with subplots
-fig, axes = plt.subplots(len(energy_channels), 1, figsize=(16, 40), sharex=True)
+fig, axes = plt.subplots(len(energy_channels_A), 1, figsize=(16, 40), sharex=True)
 
 # Loop through each energy channel
 for i, ax in enumerate(axes.flat):
   # Create the scatter plot on the current subplot
   # divide by 1000 for keV to compare to Zhao 2018
   if FESA_A is not None:
-      subplot = ax.scatter(Epoch_A, L_A, c=FESA_A[:, i]/1000, cmap=custom_cmap, norm=norm)
+      subplot = ax.scatter(Epoch_A, Lm_eq_A_interp, c=FESA_A[:, i]/1000, cmap=custom_cmap, norm=norm)
   if FESA_B is not None:
-      subplot = ax.scatter(Epoch_B, L_B, c=FESA_B[:, i]/1000, cmap=custom_cmap, norm=norm)
+      subplot = ax.scatter(Epoch_B, Lm_eq_B_interp, c=FESA_B[:, i]/1000, cmap=custom_cmap, norm=norm)
 
   # Add labels and title
   ax.set_ylabel('L', fontsize=textsize)
-  ax.set_title(f'RBSP-B REPT {energy_channels[i]:.2f} MeV Electron Spin-Averaged Flux', fontsize=textsize)
-
+  ax.set_title(f'RBSP REPT {energy_channels_A[i]:.2f} MeV Electron Spin-Averaged Flux', fontsize=textsize)
+  if min_epoch is not None and max_epoch is not None:
+        ax.set_xlim(min_epoch, max_epoch) 
   ax.tick_params(axis='both', which='major', labelsize=textsize)
+  ax.set_yticks(np.arange(2, 8, 1))  # Set ticks from 2 to 7 with interval 1
   ax.set_ylim(2, 7)
   ax.grid(True)
 
@@ -157,8 +160,8 @@ ax.set_xlabel('UTC', fontsize=textsize)
 fig.suptitle('April 21-26, 2017 RBSP-B REPT Data', fontsize=textsize+4, y=0.9)
 
 # Remove extra subplots if there aren't enough energy channels
-if len(energy_channels) < len(axes.flat):
-  for ax in axes.flat[len(energy_channels):]:
+if len(energy_channels_A) < len(axes.flat):
+  for ax in axes.flat[len(energy_channels_A):]:
     fig.delaxes(ax)
 
 # Create a single colorbar outside the loop for efficiency
@@ -171,4 +174,3 @@ cbar.ax.tick_params(labelsize=textsize)
 
 # Show the plot
 plt.show()
-'''
