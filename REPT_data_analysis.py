ee52b1fa8eaa9d843ea9a6973e39bab8eab66c1c
Will Teague <wzt0020@auburn.edu>
2025-03-18 13:45:30 -0500

    Calculated K from IRBEM, fit, and found alpha
    calculated K from X_j=I from IRBEM, fit to exponential and power law, wend with linear interpolation method. interpolated to calculated alpha for K=0.1, compared to IRBEM AlphaofK, plotted. Additionally, saved key matrices in separate file for recall.

--- REPT_data_analysis.py
+++ REPT_data_analysis.py
@@ -7,6 +7,7 @@ import scipy.constants as sc
 from spacepy.time import Ticktock
 from spacepy.coordinates import Coords
 import spacepy.irbempy as irbem
+from scipy.optimize import curve_fit
 # Plotting
 from datetime import datetime, timedelta
 import math
@@ -17,9 +18,15 @@ from matplotlib import colors
 # Import functions
 from analysis_functions import process_flux_data
 from analysis_functions import process_ephem_data
+from analysis_functions import interpolate_Ephem
 from analysis_functions import get_Omni
-from analysis_functions import find_apogee_times
+from analysis_functions import extend_alpha
 from analysis_functions import find_perigee_times
+from analysis_functions import find_apogee_times
+from analysis_functions import fit_K_v_alpha
+from analysis_functions import exponential_fit
+from analysis_functions import power_law_fit
+from analysis_functions import find_alpha
 
 # Import the latest version of OMNI data
 #from spacepy import toolbox as tb
@@ -28,26 +35,25 @@ from analysis_functions import find_perigee_times
 # Initialize global variables
 textsize = 16
 Re = 6378.137 #Earth's Radius
-Mu_select = 4000 # MeV/G
-K_select = 0.10 # R_E*G^(1/2)
+Mu_set = 4000 # MeV/G
+K_set = 0.10 # R_E*G^(1/2)
 
 # Conversions
 # electron mass in MeV is (m_e [kg] * c^2 [m^2/s^2]) [J] / (sc.eV [J/eV] * 10^6 [eV/MeV])
 electron_mass_mev = sc.electron_mass * sc.c**2 / (sc.electron_volt * 1e6)
 # B_local, B_min, B_mirr are in nT: 1 nT = 10^-5 G
-# 1 R_E*T^(-1/2) = 0.01 R_E*G^(-1/2)
 
-# Start main class
+#%% Start main class
 if __name__ == '__main__':
 #%% Folder containing CDF files
-    folder_path_l2 = "C:/Users/Will/Box/Multipoint_Box/REPT Data/April 2017 Storm/l2/"
+    folder_path_l2 = "C:/Users/wzt0020/Box/Multipoint_Box/REPT Data/April 2017 Storm/l2/"
     if not os.path.exists(folder_path_l2):
         raise FileNotFoundError(f"Error: Folder path not found: {folder_path_l2}")
-    folder_path_l3 = "C:/Users/Will/Box/Multipoint_Box/REPT Data/April 2017 Storm/l3/"
+    folder_path_l3 = "C:/Users/wzt0020/Box/Multipoint_Box/REPT Data/April 2017 Storm/l3/"
     if not os.path.exists(folder_path_l3):
         raise FileNotFoundError(f"Error: Folder path not found: {folder_path_l3}")
     
-    ephemeris_path = "C:/Users/Will/Box/Multipoint_Box/REPT Data/April 2017 Storm/ephemeris/"
+    ephemeris_path = "C:/Users/wzt0020/Box/Multipoint_Box/REPT Data/April 2017 Storm/ephemeris/"
     if not os.path.exists(ephemeris_path):
         raise FileNotFoundError(f"Error: Ephemeris path not found: {ephemeris_path}")
     
@@ -90,26 +96,23 @@ if __name__ == '__main__':
     print("Processing Ephemeris Data:")
     Epoch_ephem_A, alpha_ephem_A, Lm_ephem_A, Lstar_ephem_A, K_ephem_A = process_ephem_data(ephem_file_paths_A)
     Epoch_ephem_B, alpha_ephem_B, Lm_ephem_B, Lstar_ephem_B, K_ephem_B = process_ephem_data(ephem_file_paths_B)
-    
+   
     # Interpolate Ephemeris data for RBSP times
-    def interpolate_Ephem(Epoch, Epoch_ephem, Lm_ephem, Lstar_ephem, K_ephem):
-        Epoch_float = [epoch.timestamp() for epoch in Epoch]
-        Epoch_ephem_float = [epoch_ephem.timestamp() for epoch_ephem in Epoch_ephem]
-        Lm_interp = np.interp(Epoch_float, Epoch_ephem_float, Lm_ephem)
-        
-        Lstar_interp = np.zeros((len(Epoch_float), Lstar_ephem.shape[1]))
-        K_interp = np.zeros((len(Epoch_float), K_ephem.shape[1]))
-        for lstar_col in range(Lstar_ephem.shape[1]):
-            Lstar_interp[:,lstar_col] = np.interp(Epoch_float, Epoch_ephem_float, Lstar_ephem[:,lstar_col])
-        for k_col in range(K_interp.shape[1]):
-            K_interp[:,k_col] = np.interp(Epoch_float, Epoch_ephem_float, K_ephem[:,k_col])
-        return Lm_interp, Lstar_interp, K_interp
-    
-    print("Interpolating Ephemeris Data:")
+    print("Interpolating Ephemeris Data")
     Lm_interp_A, Lstar_interp_A, K_interp_A = interpolate_Ephem(Epoch_A, Epoch_ephem_A, Lm_ephem_A, Lstar_ephem_A, K_ephem_A)
     Lm_interp_B, Lstar_interp_B, K_interp_B = interpolate_Ephem(Epoch_B, Epoch_ephem_B, Lm_ephem_B, Lstar_ephem_B, K_ephem_B)
     
+#%% Load Saved Data
+    '''
+    loaded_data = np.load('vital_data.npz')
     
+    # Access the loaded variables
+    results_A = loaded_data['results_A']
+    results_B = loaded_data['results_B']
+    alphaofK_A = loaded_data['alphaofK_A']
+    alphaofK_B = loaded_data['alphaofK_B']
+    '''
+
 #%% Obtain Omni Information & prepare for calculating K and L*
     # Set up for IRBEM Calculations
     time_A = Ticktock(Epoch_A, 'UTC')
@@ -121,55 +124,73 @@ if __name__ == '__main__':
     omnivals_refined_B = get_Omni(Epoch_B, Position_B)
     
     print("Calculating Mu (RBSP-A)")
+    alpha_A_extend = extend_alpha(alpha_A)
+    
     B_A = irbem.get_Bfield(time_A, position_A, extMag=extMag, omnivals=omnivals_refined_A)
     Blocal_A, Bvec_A = B_A["Blocal"], B_A["Bvec"]
-    energy_grid, alpha_grid, blocal_grid = np.meshgrid(energy_channels_A, np.deg2rad(alpha_A[0:9]), Blocal_A*1e-5, indexing='ij')
+    energy_grid, alpha_grid, blocal_grid = np.meshgrid(energy_channels_A, np.deg2rad(alpha_A_extend), Blocal_A*1e-5, indexing='ij')
     Mu_A = (energy_grid**2 + 2 * energy_grid * electron_mass_mev) * np.sin(alpha_grid)**2 / (2 * electron_mass_mev * blocal_grid)
-    #Mu_A = (energy_channels_A^2+2*energy_channels_A*sc.electron_mass*sc.c^2)*np.sin(alpha)/(2*sc.electron_mass*sc.c^2*Blocal_A) 
     
     print("Calculating Mu (RBSP-B)")
+    alpha_B_extend = extend_alpha(alpha_B)
+    
     B_B = irbem.get_Bfield(time_B, position_B, extMag=extMag, omnivals=omnivals_refined_B)
     Blocal_B, Bvec_B = B_B["Blocal"], B_B["Bvec"]
-    energy_grid, alpha_grid, blocal_grid = np.meshgrid(energy_channels_B, np.deg2rad(alpha_B[0:9]), Blocal_B*1e-5, indexing='ij')
+    energy_grid, alpha_grid, blocal_grid = np.meshgrid(energy_channels_B, np.deg2rad(alpha_B_extend), Blocal_B*1e-5, indexing='ij')
     Mu_B = (energy_grid**2 + 2 * energy_grid * electron_mass_mev) * np.sin(alpha_grid)**2 / (2 * electron_mass_mev * blocal_grid)
 
 
     print("Calculating L* (RBSP-A)")
-    #results_A = irbem.get_Lstar(time_A, position_A, alpha=alpha_A[0:9], extMag=extMag, omnivals=omnivals_refined_A)
+    results_A = irbem.get_Lstar(time_A, position_A, alpha=alpha_A_extend, extMag=extMag, omnivals=omnivals_refined_A)
     Bmin_A, Bmirr_A, Lm_A, Lstar_A, MLT_A, Xj_A = results_A["Bmin"], results_A["Bmirr"], results_A["Lm"], results_A["Lstar"], results_A["MLT"], results_A["Xj"]
-    if len(Bmin_A.shape) == 1:
-        Bmin_A = Bmin_A.reshape(len(Bmin_A), 1)
-    Bmirr_A = np.concatenate((Bmirr_A[:,:-1], Bmirr_A[:, ::-1]), axis=1)
-    Lm_A    = np.concatenate((Lm_A[:,:-1], Lm_A[:, ::-1]), axis=1)
-    Lstar_A = np.concatenate((Lstar_A[:,:-1], Lstar_A[:, ::-1]), axis=1)
-    Xj_A    = np.concatenate((Xj_A[:,:-1], Xj_A[:, ::-1]), axis=1)
     
-    # Find when Lstar is NaN, typically indicating apogee
-    apogee_times_A = find_apogee_times(Lstar_A, Epoch_A)
-    # Find local maxima, indicating perigee
+    # Find when Lstar is NaN, typically indicating perigee
     perigee_times_A = find_perigee_times(Lstar_A, Epoch_A)
+    # Find local maxima, indicating apogee
+    apogee_times_A = find_apogee_times(Lstar_A, Epoch_A)
     
     print("Calculating K (RBSP-A)")
-    K_A = Xj_A*100 * np.sqrt(Bmin_A*1e-5) * np.sqrt(Bmirr_A*1e-5) # R_E*G^(1/2)
+    K_A = Xj_A * np.sqrt(Bmirr_A*1e-5) # R_E*G^(1/2)
+    # Find equation of K v alpha at each time point (assume exponential)
+    K_alpha_fit_A = fit_K_v_alpha(K_A, alpha_A_extend)
+    # Find alpha at each time point given a set K
+    alpha_set_A = find_alpha(K_set, K_A, alpha_A_extend)   
+    #np.isnan(alpha_set_A).sum()
+    
+    alphaofK_A = irbem.AlphaOfK(time_A, position_A, K=K_set, extMag=extMag, omnivals=omnivals_refined_A)
+    #np.isnan(alphaofK_A).sum()
     
     print("Calculating L* (RBSP-B)")
-    #results_B = irbem.get_Lstar(time_B, position_B, alpha=alpha_B[0:9], extMag=extMag, omnivals=omnivals_refined_B)
+    results_B = irbem.get_Lstar(time_B, position_B, alpha=alpha_B_extend, extMag=extMag, omnivals=omnivals_refined_B)
     Bmin_B, Bmirr_B, Lm_B, Lstar_B, MLT_B, Xj_B = results_B["Bmin"], results_B["Bmirr"], results_B["Lm"], results_B["Lstar"], results_B["MLT"], results_B["Xj"]
-    if len(Bmin_B.shape) == 1:
-        Bmin_B = Bmin_B.reshape(len(Bmin_B), 1)
-    Bmirr_B = np.concatenate((Bmirr_B[:,:-1], Bmirr_B[:, ::-1]), axis=1)
-    Lm_B    = np.concatenate((Lm_B[:,:-1], Lm_B[:, ::-1]), axis=1)
-    Lstar_B = np.concatenate((Lstar_B[:,:-1], Lstar_B[:, ::-1]), axis=1)
-    Lstar_B_nan = np.where(np.all(np.isnan(Lstar_B), axis=1))[0]
-    Xj_B    = np.concatenate((Xj_B[:,:-1], Xj_B[:, ::-1]), axis=1)
 
-    # Find when Lstar is NaN, typically indicating apogee
-    apogee_times_B = find_apogee_times(Lstar_B, Epoch_B)
-    # Find local maxima, indicating perigee
-    perigee_times_B = find_perigee_times(Lstar_B, Epoch_B)
+    # Find when Lstar is NaN, typically indicating perigee
+    perigee_times_A = find_perigee_times(Lstar_B, Epoch_B)
+    # Find local maxima, indicating apogee
+    apogee_times_A = find_apogee_times(Lstar_B, Epoch_B)
 
     print("Calculating K (RBSP-B)")
-    K_B = Xj_B*100 * np.sqrt(Bmin_B*1e-5) * np.sqrt(Bmirr_B*1e-5) # R_E*G^(1/2)
+    K_B = Xj_B * np.sqrt(Bmirr_B*1e-5) # R_E*G^(1/2)
+    # Find equation of K v alpha at each time point (assume exponential)
+    K_alpha_fit_B = fit_K_v_alpha(K_B, alpha_B_extend)
+    # Find alpha at each time point given a set K
+    alpha_set_B = find_alpha(K_set, K_B, alpha_B_extend) 
+    
+    alphaofK_B = irbem.AlphaOfK(time_B, position_B, K=K_set, extMag=extMag, omnivals=omnivals_refined_B)
+
+#%% Save chosen data
+    '''
+    # Create a dictionary to store the variables
+    data_to_save = {
+        'results_A': results_A,
+        'results_B': results_B,
+        'alphaofK_A': alphaofK_A,
+        'alphaofK_B': alphaofK_B
+    }
+    
+    # Save the dictionary to a .npz file (NumPy zip archive)
+    np.savez('vital_data.npz', **data_to_save)
+    '''
 
 #%% Plots
     '''
@@ -194,6 +215,8 @@ if __name__ == '__main__':
     fig.autofmt_xdate()
     plt.show()
     
+    
+    
     # Plot ephemeris file data against calculated L* data for RBSP A
     fig, ax = plt.subplots()  
     ax.scatter(Lstar_interp_A[:,10], Lstar_A, s=2, color = 'blue')
@@ -206,6 +229,8 @@ if __name__ == '__main__':
     ax.grid(True)
     ax.set_aspect('equal')
     plt.show()
+    
+    
     
     # Plot ephemeris file data against calculated L* data for RBSP B
     fig, ax = plt.subplots()  
@@ -220,6 +245,8 @@ if __name__ == '__main__':
     ax.set_aspect('equal')
     plt.show()
     '''
+    
+    
     '''
     # Plot calculated Mu data against L* for RBSP A for each pitch angle
     fig, axes = plt.subplots(9, 1, figsize=(12, 30), sharex=True)
@@ -244,6 +271,8 @@ if __name__ == '__main__':
     plt.subplots_adjust(right=0.95, top=0.95)
     fig.suptitle(f"RBSP-A: {Epoch_A[4138]} to {Epoch_A[5655]}", fontsize=textsize + 4) # Add figure title
     plt.show()
+    
+    
     
     # Plot Mu vs time for each energy channel at each pitch angle for RBSP-A
     fig, axes = plt.subplots(9, 1, figsize=(12, 30), sharex=True)
@@ -271,47 +300,100 @@ if __name__ == '__main__':
     fig.suptitle(f"RBSP-A: Mu V Time", fontsize=textsize + 4) # Add figure title
     plt.show()
     
+    
+    
     # Plot ephemeris file data and calculated K data for RBSP A&B
     # setting pitch angle, for all time
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(15, 5))
-    ax1.scatter(Epoch_A, K_interp_A[:, 2], s=5)
-    ax1.scatter(Epoch_B, K_interp_B[:, 2], s=5)
+    # 25: interp = 13, calc = 2, 14
+    # 45: interp = 9, calc = 4, 12
+    # 80: interp = 2, calc = 7, 9
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 5))
+    ax1.scatter(Epoch_A, K_interp_A[:, 9], s=5)
+    ax1.scatter(Epoch_B, K_interp_B[:, 9], s=5)
     ax1.set_title("Ephemeris K", fontsize=textsize)
     ax1.set_ylim(-0.1, 3)
     ax1.set_yticks(np.arange(0, 4, 1))
     ax1.set_ylabel("K", fontsize=textsize)
     ax1.tick_params(axis='both', labelsize=textsize)  # Set tick label size
-    ax2.scatter(Epoch_A, K_A[:, 7], s=5, color='tab:blue')
-    ax2.scatter(Epoch_A, K_A[:, 9], s=5, color='tab:blue')
-    ax2.scatter(Epoch_B, K_B[:, 7], s=5, color='tab:orange')
-    ax2.scatter(Epoch_B, K_B[:, 9], s=5, color='tab:orange')
+    ax2.scatter(Epoch_A, K_A[:, 4], s=5, color='tab:blue')
+    ax2.scatter(Epoch_A, K_A[:, 12], s=5, color='tab:blue')
+    ax2.scatter(Epoch_B, K_B[:, 4], s=5, color='tab:orange')
+    ax2.scatter(Epoch_B, K_B[:, 12], s=5, color='tab:orange')
     ax2.set_title("Calculated K", fontsize=textsize)
     ax2.xaxis.set_major_locator(matplotlib.dates.HourLocator(interval=6))
     ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m-%d %H'))
     plt.xticks(rotation=45, ha='right', fontsize=textsize)  # Rotate 45 degrees, align right
-    ax2.set_ylim(-0.1, 3)
+    #ax2.set_ylim(-0.1, 3)
     ax2.set_yticks(np.arange(0, 4, 1))
     ax2.set_xlabel("Time", fontsize=textsize)
     ax2.set_ylabel("K", fontsize=textsize)
     ax2.tick_params(axis='both', labelsize=textsize)  # Set tick label size
-    fig.suptitle("Pitch Angle = 80 degrees", fontsize=textsize)
+    fig.suptitle("Pitch Angle = 45 degrees", fontsize=textsize)
     plt.show()
+    
+    
     
     # Plot ephemeris file data and calculated K data for RBSP A&B
     # setting time point, for all pitch angles
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(15, 5))
-    ax1.scatter(alpha_ephem_A, K_interp_A[500, :])
-    ax1.scatter(alpha_ephem_B, K_interp_B[500, :])
-    ax1.set_title("Ephemeris K", fontsize=textsize)
-    ax1.set_ylabel("K", fontsize=textsize)
-    ax1.tick_params(axis='both', labelsize=textsize)
-    ax2.scatter(alpha_A, K_A[500, :])
-    ax2.scatter(alpha_A, K_B[500, :])
-    ax2.set_title("Calculated K", fontsize=textsize)
-    ax2.set_xlabel("Pitch Angle (degrees)", fontsize=textsize)
-    ax2.set_ylabel("K", fontsize=textsize)
-    ax2.tick_params(axis='both', labelsize=textsize)
-    fig.suptitle(f"Time = {Epoch_A[500]}", fontsize=textsize)
+    smooth_alpha = np.linspace(0, 90, 100)
+    time_index = 500
+    fig, (axA, axB) = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(20, 5))
+    scatter_calc_A = axA.scatter(alpha_A_extend, K_A[time_index, :], s=100, label="Calculated")  # Blue circles
+    exponential_fit_A = axA.plot(smooth_alpha, exponential_fit(smooth_alpha, **K_alpha_fit_A[time_index]), ls='--', linewidth=2, label="Eponential Fit")[0]
+    power_fit_A = axA.plot(smooth_alpha, power_law_fit(smooth_alpha, **K_alpha_fit_power_A[time_index]), ls=':', linewidth=3, color=exponential_fit_A.get_color(), label="Power Law Fit")[0]
+    scatter_ephem_A = axA.scatter(alpha_ephem_A, K_interp_A[time_index, :], s=100, label="Ephemeris", marker='^')
+    axA.set_title("RBSP-A", fontsize=textsize)
+    axA.set_ylabel("K", fontsize=textsize)
+    axA.tick_params(axis='both', labelsize=textsize)
+    axA.set_ylim([-0.1, 1])
+    
+    axB.scatter(alpha_B_extend, K_B[time_index, :], s=100, label="Calculated")
+    axB.plot(smooth_alpha, exponential_fit(smooth_alpha, **K_alpha_fit_B[time_index]), ls='--', linewidth=2, label="Eponential Fit")[0]
+    axB.plot(smooth_alpha, power_law_fit(smooth_alpha, **K_alpha_fit_power_B[time_index]), ls=':', linewidth=3,  color=exponential_fit_A.get_color(), label="Power Law Fit")[0]
+    axB.scatter(alpha_ephem_B, K_interp_B[time_index, :], s=100, label="Ephemeris", marker='^')
+    axB.set_title("RBSP-B", fontsize=textsize)
+    axB.set_xlabel("Pitch Angle (degrees)", fontsize=textsize)
+    axB.set_ylabel("K", fontsize=textsize)
+    axB.tick_params(axis='both', labelsize=textsize)
+    axB.set_ylim([-0.1, 1])
+    
+    # Create a single legend
+    handles = [scatter_calc_A, exponential_fit_A, power_fit_A, scatter_ephem_A] #handles from A
+    labels = [h.get_label() for h in handles]
+    
+    fig.legend(handles, labels, loc='upper right', bbox_to_anchor=(0.95, 0.95), fontsize=textsize) #create legend in figure space.
+
+    fig.suptitle(f"Time = {Epoch_A[time_index]}", fontsize=textsize)
+    plt.show()
+    
+    
+    
+    # Plot linear interpolated alpha for K=0.1, comparing to IRBEM calculations
+    fig, (axA, axB) = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(15, 7))  
+    scatter_alpha_set_A = axA.scatter(Epoch_A, alpha_set_A, s=10, label="Interpolated")
+    scatter_alphaofK_A = axA.scatter(Epoch_A, alphaofK_A, s=10, label="IRBEM")
+    scatter_alpha_set_B = axB.scatter(Epoch_B, alpha_set_B, s=10, label="Interpolated")
+    scatter_alphaofK_B = axB.scatter(Epoch_B, alphaofK_B, s=10, label="IRBEM")
+    axA.set_title("RBSP-A", fontsize=textsize)  # Top plot label
+    axB.set_title("RBSP-B", fontsize=textsize)          # Bottom plot label
+    axA.set_ylabel(r"$\alpha$", fontsize=textsize)
+    axB.set_ylabel(r"$\alpha$", fontsize=textsize)
+    axA.tick_params(axis='both', labelsize=textsize)  # Set tick label size
+    # Force labels for first and last x-axis tick marks 
+    min_epoch = datetime(1970, 1, 1) + timedelta(hours=math.floor((min_epoch - datetime(1970, 1, 1)).total_seconds() / 3600 / 12) * 12) 
+    max_epoch = datetime(1970, 1, 1) + timedelta(hours=math.ceil((max_epoch - datetime(1970, 1, 1)).total_seconds() / 3600 / 12) * 12)
+    axB.set_xlim(min_epoch, max_epoch) 
+    axB.xaxis.set_major_locator(matplotlib.dates.HourLocator(interval=12) )
+    axB.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m-%d %H'))
+    axB.set_xlabel("Time", fontsize=textsize)
+    axB.tick_params(axis='both', labelsize=textsize)  # Set tick label size
+    plt.xticks(rotation=45, ha='right', fontsize=textsize)  # Rotate 45 degrees, align right
+    # Create a single legend
+    handles, labels = axA.get_legend_handles_labels()
+    legend = fig.legend(handles, labels, loc='upper right', bbox_to_anchor=(0.95, 0.95), fontsize=textsize)
+    for handle in legend.legend_handles:
+        handle.set_sizes([100.0]) # Adjust the size as needed
+    fig.suptitle(f"Compare $\\alpha$ from interpolation to IRBEM for K=0.1", fontsize=textsize + 4) # Add figure title
     plt.show()
     '''
     
--- analysis_functions.py
+++ analysis_functions.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from spacepy import pycdf
 import spacepy.omni as omni
+from scipy.optimize import curve_fit
 
 #%% Proccess REPT CDF
 def process_flux_data(file_paths):
@@ -79,6 +80,20 @@ def process_ephem_data(ephem_file_paths):
     # Finish rading in and return data
     return Epoch_ephem, alpha_ephem, Lm_ephem, Lstar_ephem, K_ephem
 
+#%% Interpolate Ephemeris data for RBSP times
+def interpolate_Ephem(Epoch, Epoch_ephem, Lm_ephem, Lstar_ephem, K_ephem):
+    Epoch_float = [epoch.timestamp() for epoch in Epoch]
+    Epoch_ephem_float = [epoch_ephem.timestamp() for epoch_ephem in Epoch_ephem]
+    Lm_interp = np.interp(Epoch_float, Epoch_ephem_float, Lm_ephem)
+    
+    Lstar_interp = np.zeros((len(Epoch_float), Lstar_ephem.shape[1]))
+    K_interp = np.zeros((len(Epoch_float), K_ephem.shape[1]))
+    for lstar_col in range(Lstar_ephem.shape[1]):
+        Lstar_interp[:,lstar_col] = np.interp(Epoch_float, Epoch_ephem_float, Lstar_ephem[:,lstar_col])
+    for k_col in range(K_interp.shape[1]):
+        K_interp[:,k_col] = np.interp(Epoch_float, Epoch_ephem_float, K_ephem[:,k_col])
+    return Lm_interp, Lstar_interp, K_interp
+
 #%% Get Omni data for specified spacetime
 def get_Omni(time, position):      
     mag_key_mapping = {
@@ -104,10 +119,22 @@ def get_Omni(time, position):
             print(f"Warning: Key '{cdf_key}' not found in CDF data. Skipping.")
     return omnivals_refined
 
+#%% Extend Alpha for more values
+def extend_alpha(alpha):
+    alpha_extend = []
+    for i in range(len(alpha[0:9]) - 1):
+        alpha_extend.append(alpha[i])
+        intermediate_values = np.linspace(alpha[i], alpha[i+1], 4)[1:3] #4 points, get the middle 2
+        alpha_extend.extend(intermediate_values)
+    alpha_extend.append(alpha[i+1]) #add the final point.
+    
+    alpha_extend = np.array(alpha_extend)
+    return alpha_extend
+
 #%% Find times where Lstar is NaN
-def find_apogee_times(Lstar, Epoch):
+def find_perigee_times(Lstar, Epoch):
     """
-    Finds the start and stop indices and times where Lstar rows are all NaN (usually orbital apogee or missing data).
+    Finds the start and stop indices and times where Lstar rows are all NaN (usually orbital perigee or missing data).
 
     Args:
         Lstar (numpy.ndarray): The 2D NumPy array with NaN values.
@@ -118,7 +145,7 @@ def find_apogee_times(Lstar, Epoch):
     """
 
     Lstar_nan = np.where(np.all(np.isnan(Lstar), axis=1))[0]
-    apogee_times = []
+    perigee_times = []
     start_index = Lstar_nan[0]
     stop_index = Lstar_nan[0]
 
@@ -126,15 +153,15 @@ def find_apogee_times(Lstar, Epoch):
         if Lstar_nan[i] == stop_index + 1:
             stop_index = Lstar_nan[i]
         else:
-            apogee_times.append([start_index, Epoch[start_index], stop_index, Epoch[stop_index]])
+            perigee_times.append([start_index, Epoch[start_index], stop_index, Epoch[stop_index]])
             start_index = Lstar_nan[i]
             stop_index = Lstar_nan[i]
 
-    apogee_times.append([start_index, Epoch[start_index], stop_index, Epoch[stop_index]])
+    perigee_times.append([start_index, Epoch[start_index], stop_index, Epoch[stop_index]])
 
-    return np.array(apogee_times)
+    return np.array(perigee_times)
 
-def find_perigee_times(Lstar, Epoch, column_index=8, time_window=100):
+def find_apogee_times(Lstar, Epoch, column_index=8, time_window=100):
     """
     Finds the indices and times of local maxima in a specified column of a 2D Lstar array
     using an if condition, checking within a specified time window.
@@ -146,10 +173,10 @@ def find_perigee_times(Lstar, Epoch, column_index=8, time_window=100):
         time_window (int): The number of time steps to check for local maxima.
 
     Returns:
-        numpy.ndarray: A 2D NumPy array with perigee_index, perigee_time.
+        numpy.ndarray: A 2D NumPy array with apogee_index, apogee_time.
     """
 
-    perigee_times = []
+    apogee_times = []
     column_data = Lstar[:, column_index]  # Extract the specified column
     column_data[np.isnan(column_data)] = -np.inf  # Replace NaNs with negative infinity
 
@@ -161,6 +188,91 @@ def find_perigee_times(Lstar, Epoch, column_index=8, time_window=100):
                 break
 
         if is_local_max:
-            perigee_times.append([i, Epoch[i]])
+            apogee_times.append([i, Epoch[i]])
 
-    return np.array(perigee_times)
+    return np.array(apogee_times)
+
+#%% 
+def exponential_fit(x, a, b, c):
+     """Exponential function: a * exp(b * x) + c"""
+     return a * np.exp(b * x) + c
+ 
+def power_law_fit(x, a, b, c):
+    """
+    Defines a power law function with an offset: y = a * x^b + c
+
+    Args:
+        x (float or numpy.ndarray): The independent variable.
+        a (float): The scaling factor.
+        b (float): The exponent.
+        c (float): The offset.
+
+    Returns:
+        float or numpy.ndarray: The dependent variable (y).
+    """
+    return a * x**b + c
+
+def fit_K_v_alpha(K, alpha):
+    """
+    Fits an exponential function to each row of K (columns 0-8) while handling NaN values.
+
+    Args:
+        K (numpy.ndarray): 2D array with shape (num_rows, 17).
+        alpha (numpy.ndarray): 1D array representing pitch angles.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary contains the
+              fitted parameters (a, b, c) for each row.
+    """
+
+    fitted_params = []
+    alpha = alpha[0:int((K.shape[1]+1)/2-1)]
+    for row_index in range(K.shape[0]):
+        y_values = K[row_index, 0:int((K.shape[1]+1)/2-1)]  # Extract y-values for the current row
+        nan_mask = ~np.isnan(y_values)  # Create mask for NaN values
+        valid_y_values = y_values[nan_mask]  # Apply mask to y values.
+        valid_x_values = np.array(alpha)[nan_mask]  # Apply mask to x values.
+
+        if len(valid_y_values) < 3:  # Ensure there are enough values to fit.
+            fitted_params.append({'a': np.nan, 'b': np.nan, 'c': np.nan})
+            continue
+
+        try:
+            params, covariance = curve_fit(exponential_fit, valid_x_values, valid_y_values, p0=[1, -0.1, 0])
+            fitted_params.append({'a': params[0], 'b': params[1], 'c': params[2]})
+        except RuntimeError:
+
+            fitted_params.append({'a': np.nan, 'b': np.nan, 'c': np.nan})
+
+    return fitted_params
+
+#%% Find alpha given K
+def find_alpha(K_set, K, alpha):
+    if K.shape[1] != len(alpha):
+        raise ValueError("Number of columns in K must match length of alpha.")
+
+    alpha_set = np.full(K.shape[0], np.nan)  # Initialize with NaN
+
+    for time_index in range(K.shape[0]):
+        row_k = K[time_index, :]
+        nan_mask = np.isnan(row_k)
+        valid_mask = row_k <= 1
+
+        # Combine masks to exclude NaNs and values > 1
+        combined_mask = ~nan_mask & valid_mask
+
+        if np.any(combined_mask):  # Check if there are any valid values
+            valid_k = row_k[combined_mask]
+            valid_alpha = alpha[combined_mask]
+
+            # Ensure valid_k is sorted for interpolation
+            sort_indices = np.argsort(valid_k)
+            valid_k = valid_k[sort_indices]
+            valid_alpha = valid_alpha[sort_indices]
+
+            if np.min(valid_k) <= K_set <= np.max(valid_k): #make sure K_set is in range.
+                alpha_set[time_index] = np.interp(K_set, valid_k, valid_alpha)
+
+    return alpha_set
+        
+    
